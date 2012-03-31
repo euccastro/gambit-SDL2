e8d63f803310b50e298eb12cf8941ee33c46056a
Estevo <euccastro@gmail.com>
2012-03-31 14:33:16 -0400

    Wrap math functions for test.

---
+++ math.scm
@@ -0,0 +1,13 @@
+; C <math.h> functions, wrapped here just for the sake of running the
+; example in test.scm
+
+(c-declare "#include <math.h>")
+
+(define sin
+  (c-lambda (double) double "sin"))
+
+(define cos
+  (c-lambda (double) double "cos"))
+
+(define M_PI
+  ((c-lambda () double "___result = M_PI;")))
